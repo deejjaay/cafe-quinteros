94f975d2b2f87a5a56f8eb3f51b4d769e75cb9b2
Denverjack <denverlagasca@gmail.com>
2024-12-10 17:07:15 +0800

    Fix sliding animation and Join our community section alignment

--- app/(pages)/(home)/_sections/JoinOurCommunity.tsx
+++ app/(pages)/(home)/_sections/JoinOurCommunity.tsx
@@ -1,41 +1,80 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import Map from '../../../../public/images/discover/mobile/map.png';
 import Image from 'next/image';
+import { z } from  'zod';
 
 const JoinOurCommunityPage = () => {
+    const [whatsappNumber, setWhatsappNumber] = useState('');
+    const [error, setError] = useState('');
+
+    const schema = z.object({
+        whatsappNumber: z
+            .string()
+            .min(10, 'Please provide a valid WhatsApp number.')
+            .regex(/^\d{10}$/, 'Invalid WhatsApp number, should be 10 digits')
+    });
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            schema.parse({ whatsappNumber });
+            setError('');
+            console.log('Form Submitted: ', { whatsappNumber });
+            setWhatsappNumber('');
+        } catch (err) {
+            if (err instanceof z.ZodError) {
+                setError(err.errors[0].message);
+            }
+        }
+    };
+    
     return (
         <section className="bg-cover bg-center bg-opacity-70 bg-[url('/images/discover/mobile/map.png')]">
             <div className='bg-brown opacity-90'>
-                <div className='container gap-[80px] md:py-custom-60 py-custom-40 md:flex-wrap xl:px-[4rem] grid xl:grid-cols-2'>
-                    <div className="md:flex md:flex-wrap">
-                        <div className="md:flex md:flex-wrap md:w-full mb-[48px]">
-                            <h6 className="w-full text-montserrat_bold_24 text-white mb-[16px]">
-                                Join Our Cafe Community
-                            </h6>
-                            <p className="text-montserrat_regular_16 text-white opacity-60">
-                                Get daily breakfast and lunch specials! Please provide us your WhatsApp number.
-                            </p>
+                <div className='container xl:max-w-xl'>
+                    <div className='gap-[8rem] py-[4rem] md:py-[6rem] md:flex-wrap grid xl:grid-cols-2'>
+                        <div className="md:flex md:flex-wrap">
+                            <div className="md:flex md:flex-wrap md:w-full mb-[4.8rem]">
+                                <h6 className="w-full text-montserrat_bold_24 text-white mb-[1.6rem]">
+                                    Join Our Cafe Community
+                                </h6>
+                                <p className="text-montserrat_regular_16 text-white opacity-60">
+                                    Get daily breakfast and lunch specials! Please provide us your WhatsApp number.
+                                </p>
+                            </div>
+
+                            <form onSubmit={handleSubmit} className="w-full flex flex-wrap items-start gap-[16px] md:flex-nowrap xl:items-end">
+                                <div className="w-full flex flex-col md:flex-row gap-[1.6rem]">
+                                    <div className="flex flex-col w-full">
+                                        <input
+                                            className="w-full h-[6.4rem] rounded-[.8rem] px-[1.6rem] text-montserrat_regular_16"
+                                            type="text"
+                                            placeholder="WhatsApp number*"
+                                            value={whatsappNumber}
+                                            onChange={(e) => setWhatsappNumber(e.target.value)}
+                                        />
+                                        {error && <p className="text-[#c26057] text-base mt-[.5rem]">{error}</p>}
+                                    </div>
+                                    <button 
+                                        type="submit" 
+                                        className="w-full px-[1.6rem] h-[6.4rem] rounded-[.8rem] text-white bg-primary text-montserrat_bold_16_1 whitespace-nowrap md:w-auto"
+                                    >
+                                        Join Community
+                                    </button>
+                                </div>
+                            </form>
                         </div>
 
-                        <div className="w-full flex flex-wrap gap-[16px] md:flex-nowrap xl:items-end">
-                            <input
-                                className="w-full h-[64px] rounded-[8px] pl-3"
-                                type="text"
-                                placeholder="WhatsApp number*"
-                            />
-                            <button className="w-full px-[16px] h-[64px] rounded-[8px] text-white bg-primary text-montserrat_bold_16_1 whitespace-nowrap md:w-auto">
-                                Join Community
-                            </button>
-                        </div>
+                        <Image 
+                            src={Map} 
+                            width={352} 
+                            height={410}
+                            alt={'Map'} 
+                            className="w-full object-cover rounded-[8px] md:h-[410px] xl:h-[251px]"
+                        />
                     </div>
-
-                    <Image 
-                        src={Map} 
-                        width={352} 
-                        height={410}
-                        alt={'Map'} 
-                        className="w-full object-cover rounded-[8px] md:h-[410px] xl:h-[251px]"
-                    />
                 </div>
             </div>
         </section>
--- app/(pages)/(home)/_sections/WordsFromCustomers.tsx
+++ app/(pages)/(home)/_sections/WordsFromCustomers.tsx
@@ -3,19 +3,19 @@ import Feedbacks from "../_components/Feedback";
 const WordsFromCustomersPage = () => {
   return (
     <section className="md:py-custom-60 py-custom-40 bg-gray">
-        <div className="container xl:max-w-xl">
+        <div className="container md:px-0 xl:max-w-xl">
             <h6 className="mb-[1.6rem] text-montserrat_bold_28 md:text-center md:text-montserrat_bold_40 lg:text-montserrat_bold_46 xl:text-montserrat_bold_48">
-            Words From Our Customers
+                Words From Our Customers
             </h6>
-            <p className="mb-[2.4rem] text-montserrat_regular_16 opacity-60 md:text-center">
-            Our customers love our food and friendly vibe! Discover why we’re the top choice for El Salvadoran breakfast and lunch.
+            <p className="mb-[2.4rem] text-montserrat_regular_16 opacity-60 md:text-center md:px-[4rem] lg:px-[10rem]">
+                Our customers love our food and friendly vibe! Discover why we’re the top choice for El Salvadoran breakfast and lunch.
             </p>
-        </div>
-        <div className="relative overflow-hidden mx-[1.6rem] md:mx-0 xl:mx-[12rem] xl:max-w-xl">
-            <div className="flex flex-col md:flex-row md:flex-wrap gap-6">
-                <Feedbacks />
+            <div className="relative overflow-hidden md:mx-0 xl:mx-[12rem]">
+                <div className="flex flex-col md:flex-row md:flex-wrap gap-6">
+                    <Feedbacks />
+                </div>
+                <div className="hidden xl:block fade-effect"></div>
             </div>
-            <div className="hidden xl:block fade-effect"></div>
         </div>
     </section>
   );
